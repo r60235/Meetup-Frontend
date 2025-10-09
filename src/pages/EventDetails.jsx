1c6174fb1003b6921c91ff7b84ee37cae7dff948
admin <admin-email@example.com>
2025-10-09 17:53:11 +0530

    feat: Added EventDetails page

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -1,14 +1,27 @@
-const EventDetails = ()=>{
-    
+import { useParams } from "react-router-dom";
+import useFetch from "../useFetch";
+import Header from "../components/Header";
+
+const EventDetails = () => {
+    const { id } = useParams();
+    const { data: event, loading, error } = useFetch(`http://localhost:5001/event/${id}`);
+
+    if (error) return <p>Error: {error}</p>;
+
+    // missing event
+    if (!event) return <p>No event found.</p>;
 
     return (
-        <div></div>
-    )
-
-} 
-
-export  const  findEventByEventTitleAndTags =  (titleOrTag)=>{
-        console.log(titleOrTag);
-}
+        <div className="bg-light">
+            <div className="container py-3">
+                <Header />
+                <div className="border-bottom border-dark mb-4"></div>
+                {loading && <p>Loading...</p>}
+                
+                
+            </div>
+        </div>
+    );
+};
 
 export default EventDetails;
